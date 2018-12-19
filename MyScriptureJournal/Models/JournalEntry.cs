ea1588d08d931ac3d4d3431b408cd55f830f6c8f
Brad R. Allen <bradrallen@gmail.com>
2018-12-19 00:06:05 -0700

    Final Commit

--- MyScriptureJournal/Models/JournalEntry.cs
+++ MyScriptureJournal/Models/JournalEntry.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,23 @@ namespace MyScriptureJournal.Models
     public class JournalEntry
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Book name cannot be longer than 50 characters.")]
         public string Book { get; set; }
+
+        
         public int Chapter { get; set; }
+      
+
         public int Verse { get; set; }
+
+        [Required]
         public string Note { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Date Added")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime DateAdded { get; set; }
 
     }
--- MyScriptureJournal/Pages/Journal/Index.cshtml.cs
+++ MyScriptureJournal/Pages/Journal/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MyScriptureJournal.Models;
 
@@ -18,11 +19,57 @@ namespace MyScriptureJournal.Pages.Journal
             _context = context;
         }
 
-        public IList<JournalEntry> JournalEntry { get;set; }
+       
+      
+        public string CurrentFilter { get; set; }
+        public string BookSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentSort { get; set; }
+        public PaginatedList<JournalEntry> JournalEntry { get; set; }
 
-        public async Task OnGetAsync()
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            JournalEntry = await _context.JournalEntry.ToListAsync();
+            BookSort = String.IsNullOrEmpty(sortOrder) ? "book_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            CurrentSort = sortOrder;
+
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<JournalEntry> journalIQ = from j in _context.JournalEntry
+                                            select j;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                journalIQ = journalIQ.Where(j => j.Book.Contains(searchString) || j.Note.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "book_desc":
+                    journalIQ = journalIQ.OrderByDescending(j => j.Book);
+                    break;
+                case "date_desc":
+                    journalIQ = journalIQ.OrderByDescending(j => j.DateAdded);
+                    break;
+                default:
+                    journalIQ = journalIQ.OrderBy(j => j.DateAdded);
+                    break;
+            }
+
+            int pageSize = 5;
+
+            JournalEntry = await PaginatedList<JournalEntry>.CreateAsync(journalIQ
+                .AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
